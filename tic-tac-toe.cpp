7f1d5726a28550b7b2c1207d8cf8af20166f2537
haostonh <hsthuynh@ucdavis.edu>
2022-01-05 14:10:26 -0800

    get number of players

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
 
+int getPlayers() {
+    int numPlayers = 0;
+    
+    while (numPlayers != 1 || numPlayers != 2) {
+        std::cout << "Enter the number of players that will be playing: " << std::endl;
+        std::cin >> numPlayers;
+        if (numPlayers != 1 || numPlayers != 2) {
+            std::cout << "Invalid Number of Players, Try Again" << std::endl;
+        }
+    }
+    
+    return numPlayers;
+}
+
 int main() {
+    int numPlayers;
+
+    std::cout << "Welcome to Tic Tac Toe!\n\n" << std::endl;
+    numPlayers = getPlayers();
+    
+
     return 0;
 }
