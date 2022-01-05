27f3cdfeb4fc6bf75f5f811d39b14ed1258c6706
haostonh <hsthuynh@ucdavis.edu>
2022-01-05 14:27:26 -0800

    fix indentation

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 
 int GetPlayers() {
-    int num_players = 0;
+  int num_players = 0;
     
-    while (num_players != 1 && num_players != 2) {
-        std::cout << "Enter the number of players that will be playing: " << std::endl;
-        std::cin >> num_players;
+  while (num_players != 1 && num_players != 2) {
+    std::cout << "Enter the number of players that will be playing: " << std::endl;
+    std::cin >> num_players;
 
-        if (num_players != 1 && num_players != 2) {
-            std::cout << "Invalid Number of Players, Try Again" << std::endl;
-        }
+    if (num_players != 1 && num_players != 2) {
+        std::cout << "Invalid Number of Players, Try Again" << std::endl;
     }
-    
-    return num_players;
+  }
+  
+  return num_players;
 }
 
 int main() {
-    int num_players;
+  int num_players;
 
-    std::cout << "Welcome to Tic Tac Toe!\n\n" << std::endl;
-    num_players = GetPlayers();
-    
+  std::cout << "Welcome to Tic Tac Toe!\n\n" << std::endl;
+  num_players = GetPlayers();
+  
 
-    return 0;
+  return 0;
 }
