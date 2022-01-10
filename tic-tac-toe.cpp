0e9ad6faf2a21d1bf2f46ec7d92a196e1d8bdeea
haostonh <hsthuynh@ucdavis.edu>
2022-01-10 14:37:30 -0800

    Finished bottom tile checks

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -84,6 +84,35 @@ int BoardChecker(char board[3][3]) {
           break;
         }
       }
+
+      // Check bottom left corner tile
+      if ((row - 1 >= 0) && (row - 2 >= 0) && (col + 1 < 3) && (col + 2 < 3)) {
+        if (((current_symbol == board[row][col + 1]) && (current_symbol == board[row][col + 2])) ||
+        ((current_symbol == board[row - 1][col]) && (current_symbol == board[row - 2][col])) ||
+        ((current_symbol == board[row - 1][col + 1]) && (current_symbol == board[row - 2][col + 2]))) {
+          three_in_a_row = 1;
+          break;
+        }
+      }
+
+      // Check bottom center tile
+      if ((row - 1 >= 0) && (row - 2 >= 0) && (col - 1 >= 0) && (col + 1 < 3)) {
+        if (((current_symbol == board[row][col - 1]) && (current_symbol == board[row][col + 1])) ||
+        ((current_symbol == board[row - 1][col]) && (current_symbol == board[row - 2][col]))) {
+          three_in_a_row = 1;
+          break;
+        }
+      }
+
+      // Check bottom right tile
+      if ((row - 1 >= 0) && (row - 2 >= 0) && (col - 1 >= 0) && (col - 2 >= 0)) {
+        if (((current_symbol == board[row][col - 1]) && (current_symbol == board[row][col - 2])) ||
+        ((current_symbol == board[row - 1][col]) && (current_symbol == board[row - 2][col])) ||
+        ((current_symbol == board[row - 1][col - 1]) && (current_symbol == board[row - 2][col - 2]))) {
+          three_in_a_row = 1;
+          break;
+        }
+      }
     }
 
     if (three_in_a_row == true) {
