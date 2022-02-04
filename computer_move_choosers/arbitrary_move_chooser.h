639364d1f14f817562bdded1aaf32a67d113e15c
haostonh <hsthuynh@ucdavis.edu>
2022-02-03 19:27:56 -0800

    Created simple random generator for coords

---
+++ computer_move_choosers/arbitrary_move_chooser.h
@@ -0,0 +1,15 @@
+#include<random>
+
+int ComputerBoardChooser(char board[3][3], char computer_letter) {
+  bool invalid_coordinate = true;
+  int x_coordinate, y_coordinate;
+
+  do {
+    x_coordinate, y_coordinate = rand() % 3;
+    if (board[x_coordinate][y_coordinate] == ' ') {
+      invalid_coordinate = false;
+      board[x_coordinate][y_coordinate] = computer_letter;
+    }
+  } 
+  while (invalid_coordinate);
+}
