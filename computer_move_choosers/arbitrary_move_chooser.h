b312bfcc61a6d95272aeceb40f7fd04c7cc7cfe0
haostonh <hsthuynh@ucdavis.edu>
2022-02-08 16:51:28 -0800

    add spacing

--- computer_move_choosers/arbitrary_move_chooser.h
+++ computer_move_choosers/arbitrary_move_chooser.h
@@ -6,8 +6,8 @@ int ComputerBoardChooser(char board[3][3], char computer_letter) {
   int y_coordinate = 0;
 
   do {
-
     x_coordinate, y_coordinate = rand() % 3;
+    
     if (board[x_coordinate][y_coordinate] == ' ') {
       invalid_coordinate = false;
       board[x_coordinate][y_coordinate] = computer_letter;
