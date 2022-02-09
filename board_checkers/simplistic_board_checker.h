bd0d5dfc45f2adb2e2617fce8749c9e16e15adf3
haostonh <hsthuynh@ucdavis.edu>
2022-02-08 16:45:15 -0800

    organize variables by type alphabetically

--- board_checkers/simplistic_board_checker.h
+++ board_checkers/simplistic_board_checker.h
@@ -1,15 +1,15 @@
 // Checks for the 8 possible winning conditions
 int BoardChecker(char board[3][3], char player_1, char player_2_or_computer) {
-  const int num_win_conditions = 8;
+  bool three_in_a_row = false;
+  char current_symbol;
   const int num_tile_coords = 3;
+  const int num_win_conditions = 8;
   const int num_x_y_coords = 2;
   int winning_rows[num_win_conditions][num_tile_coords][num_x_y_coords] = 
   {
     {{0,0},{0,1},{0,2}},{{1,0},{1,1},{1,2}},{{2,0},{2,1},{2,2}},{{0,0},{1,0},{2,0}},
     {{0,1},{1,1},{2,1}},{{0,2},{1,2},{2,2}},{{0,0},{1,1},{2,2}},{{2,0},{1,1},{0,2}}
   };
-  bool three_in_a_row = false;
-  char current_symbol;
 
   for (int index = 0; index <= num_win_conditions - 1; index++) {
     if (board[winning_rows[index][0][0]][winning_rows[index][0][1]] == board[winning_rows[index][1][0]][winning_rows[index][1][1]] &&
