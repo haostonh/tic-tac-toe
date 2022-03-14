ca334b8da95dd755e0e7c895afcb3f2adbfa1afe
haostonh <hsthuynh@ucdavis.edu>
2022-03-13 21:26:12 -0700

    fix print format of board printer func

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -48,13 +48,13 @@ int GetNumPlayers() {
 }
 
 void BoardPrinter(char board[3][3]) {
-  std::cout << "          X-Axis  " << std::endl;
-  std::cout << "         1   2   3" << std::endl;
-  std::cout << "       1 " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << std::endl;
-  std::cout << "         ---------" << std::endl;
-  std::cout << "Y-Axis 2 " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << std::endl;
-  std::cout << "         ---------" << std::endl;
-  std::cout << "       3 " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << std::endl;
+  std::cout << "          X-Axis  \n";
+  std::cout << "         1   2   3\n";
+  std::cout << "       1 " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << "\n";
+  std::cout << "         ---------" << "\n";
+  std::cout << "Y-Axis 2 " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << "\n";
+  std::cout << "         ---------" << "\n";
+  std::cout << "       3 " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << "\n";
   std::cout << std::endl;
 }
 
