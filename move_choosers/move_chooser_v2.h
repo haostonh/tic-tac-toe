092e981822f694599bc910052e4d3a71102306df
haostonh <hsthuynh@ucdavis.edu>
2022-03-15 14:40:35 -0700

    add headers needed for func

--- move_choosers/move_chooser_v2.h
+++ move_choosers/move_chooser_v2.h
@@ -1,4 +1,9 @@
 #ifndef MOVE_CHOOSERS_MOVE_CHOOSER_V2_H_
 #define MOVE_CHOOSERS_MOVE_CHOOSER_V2_H_
 
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<regex>
+
 #endif // MOVE_CHOOSERS_MOVE_CHOOSER_V2_H_
