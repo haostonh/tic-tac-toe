73d15ee2fc8f3f8de587d8a16af616eb6af34e38
haostonh <hsthuynh@ucdavis.edu>
2022-03-15 20:48:45 -0700

    separate variable initalization into two lines

--- computer_move_choosers/arbitrary_move_chooser.h
+++ computer_move_choosers/arbitrary_move_chooser.h
@@ -9,7 +9,8 @@ void ComputerBoardChooser(char board[3][3], char computer_letter) {
   int y_coordinate = 0;
 
   do {
-    x_coordinate, y_coordinate = rand() % 3;
+    x_coordinate = rand() % 3;
+    y_coordinate = rand() % 3;
 
     if (board[x_coordinate][y_coordinate] == ' ') {
       invalid_coordinate = false;
